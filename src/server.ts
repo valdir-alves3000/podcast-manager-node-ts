05c5c7110fa669ba861e5af4e9b57bbed6f7878f
valdir-alves3000 <valdiralves3000@gmail.com>
2024-05-27 11:54:30 -0300

    feat: add services

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("Hello Word!!!")
+import * as http from "node:http"
+import { getListEpisodes } from './controllers/podcasts-controller'
+
+const server = http.createServer(async (req: http.IncomingMessage, res: http.ServerResponse) => {
+  if (req.method === "GET") { await getListEpisodes(req, res) }
+})
+
+const PORT = process.env.PORT || 3333
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
---
+++ src/services/list-episodes-service.ts
@@ -0,0 +1,7 @@
+import { repositoryPodcast } from "../repositories/podcasts-repository"
+
+export const serviceListEpisodes = async () => {
+  const data = await repositoryPodcast()
+
+  return data
+}
